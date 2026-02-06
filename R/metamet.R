6da32c80555289f00ec877d61721e192ce35c17f
ADCEH\plevy <plevy@ceh.ac.uk>
2026-02-06 13:17:23 +0000

    added time handling

--- R/metamet.R
+++ R/metamet.R
@@ -1,9 +1,12 @@
 utils::globalVariables(c("errorMessage"))
 .datatable.aware <- TRUE
+# Use GMT, never BST
+Sys.setenv(TZ = "GMT")
 
-# This file defines a constructor for the metamet class and some essential S3
-# methods. A metamet object is a list of data tables, extended by
-# specifying meta data.
+# This file defines the metamet class, its constructor, and some essential S3
+# methods. A metamet object is a list of data tables - one for the met data
+# itself, and additional ones for the meta-data needed to process and interpret
+# the met data.
 
 # Constructor
 new_metamet <- function(
@@ -23,71 +26,71 @@ new_metamet <- function(
     dt_site = dt_site
   )
 
-  # Specify the class and return the metamet
+  # Specify the class and return the metamet object
   class(new_metamet) <- c("metamet", class(new_metamet))
   return(new_metamet)
 }
 
 # 1. Define the generic function
-metamet <- function(x, ...) {
+metamet <- function(dt, ...) {
   UseMethod("metamet")
 }
 
 # 2. Default method for unexpected types
 #' @export
-metamet.default <- function(x, ...) {
-  stop("Unsupported input type: ", class(x))
+metamet.default <- function(dt, ...) {
+  stop("Unsupported input type: ", class(dt))
 }
 
 # 3. Method for character strings (files)
 #' @export
-metamet.character <- function(x, dt_meta = NULL, dt_site = NULL, ...) {
-  message("Loading file: ", x)
-  dt <- data.table::fread(x)
+metamet.character <- function(dt, dt_meta = NULL, dt_site = NULL, ...) {
+  message("Loading file: ", dt)
+  dt <- data.table::fread(dt)
 
   # read metadata from file if a path is specified
   # otherwise the argument is assumed to be a data table/frame
   if ("character" %in% class(dt_meta)) {
     dt_meta <- data.table::fread(dt_meta)
   }
-  dt_meta <- data.table::setDT(dt_meta)
+  data.table::setDT(dt_meta)
 
   # and repeat for site, qc, faults, ref, ...
   if ("character" %in% class(dt_site)) {
     dt_site <- data.table::fread(dt_site)
   }
-  dt_site <- data.table::setDT(dt_site)
+  data.table::setDT(dt_site)
 
   # l_dt <- lapply(v_fname, import_campbell_data)
   # and then call the data table method
-  metamet(dt, dt_meta = dt_meta, dt_site = dt_site)
+  metamet(dt = dt, dt_meta = dt_meta, dt_site = dt_site)
 }
 
 # 4. Method for data frames
 #' @export
-metamet.data.frame <- function(x, dt_meta = NULL, dt_site = NULL, ...) {
+metamet.data.frame <- function(dt, dt_meta = NULL, dt_site = NULL, ...) {
   message("Converting data frame to data table...")
-  dt <- data.table::setDT(x)
+  data.table::setDT(dt)
 
   # read metadata from file if a path is specified
   # otherwise the argument is assumed to be a data table/frame
   if ("character" %in% class(dt_meta)) {
     dt_meta <- data.table::fread(dt_meta)
   }
-  dt_meta <- data.table::setDT(dt_meta)
+  data.table::setDT(dt_meta)
 
   # and repeat for site, qc, faults, ref, ...
   if ("character" %in% class(dt_site)) {
     dt_site <- data.table::fread(dt_site)
   }
-  dt_site <- data.table::setDT(dt_site)
+  data.table::setDT(dt_site)
 
-  metamet(dt, dt_meta = dt_meta, dt_site = dt_site)
+  metamet(dt = dt, dt_meta = dt_meta, dt_site = dt_site)
 }
 
 # 4. Method for data tables
 #' @export
-metamet.data.table <- function(x, dt_meta = NULL, dt_site = NULL, ...) {
+metamet.data.table <- function(dt, dt_meta = NULL, dt_site = NULL, ...) {
   message("Reading data table ...")
 
   # read metadata from file if a path is specified
@@ -95,23 +98,85 @@ metamet.data.table <- function(x, dt_meta = NULL, dt_site = NULL, ...) {
   if ("character" %in% class(dt_meta)) {
     dt_meta <- data.table::fread(dt_meta)
   }
-  dt_meta <- data.table::setDT(dt_meta)
+  data.table::setDT(dt_meta)
 
   # and repeat for site, qc, faults, ref, ...
   if ("character" %in% class(dt_site)) {
     dt_site <- data.table::fread(dt_site)
   }
-  dt_site <- data.table::setDT(dt_site)
+  data.table::setDT(dt_site)
 
-  new_metamet(dt = x, dt_meta = dt_meta, dt_site = dt_site)
+  mm <- new_metamet(dt = dt, dt_meta = dt_meta, dt_site = dt_site)
+  mm <- restrict(mm)
+  mm <- convert_time_char_to_posix(mm)
+  return(mm)
 }
 
-# helper function for creating metamet objects from a list of data tables or file names
+# restrict dt and dt_meta tables to only those that occur in both
+restrict <- function(mm) {
+  v_name_meta <- mm$dt_meta$name_dt
+  v_name_dt <- colnames(mm$dt)
+  # subset dt to only those that exist in metadata
+  v_name_dt <- v_name_dt[v_name_dt %in% v_name_meta]
+  mm$dt <- mm$dt[, ..v_name_dt]
 
-get_metamet_filenames <- function(
-  fname_pattern = "*Metmast_MainMet_30min*",
-  dir_in = here::here("data-raw/UK-AMO")
-) {
-  v_fname <- fs::dir_ls(dir_in, glob = fname_pattern)
-  return(v_fname)
+  # subset metadata to only variables that exist in dt
+  mm$dt_meta <- mm$dt_meta[name_dt %in% v_name_dt]
+
+  return(mm)
+}
+
+convert_time_char_to_posix <- function(mm) {
+  # get the name and format of the time variable
+  time_name <- mm$dt_meta[type == "time", name_dt]
+  time_format <- mm$dt_meta[type == "time", time_char_format]
+  # convert character variable to POSIXct
+  mm$dt[, eval(time_name) := as.POSIXct(strptime(get(time_name), time_format))]
+
+  # remove duplicate rows - sometimes occur in Campbell files
+  mm$dt <- mm$dt[!duplicated(mm$dt[, TIMESTAMP]), ]
+
+  return(mm)
+}
+
+time_average <- function(mm, avg.time = "hour") {
+  # get the name and format of the time, ws & wd variables
+  time_name <- mm$dt_meta[type == "time", name_dt]
+  wd_name <- mm$dt_meta[type == "wind direction", name_dt]
+  ws_name <- mm$dt_meta[type == "wind speed" | type == "windspeed", name_dt]
+
+  # rename time variable with openair convention
+  # mm$dt[, date := get(time_name)]
+  setnames(mm$dt, eval(time_name), "date")
+
+  # and ws & wd if present, for proper vector averaging
+  if (length(wd_name) > 0) {
+    mm$dt[, wd := get(wd_name)]
+  }
+  if (length(ws_name) > 0) {
+    mm$dt[, ws := get(ws_name)]
+  }
+
+  first_date <- min(mm$dt[, date])
+  start.date <- lubridate::floor_date(first_date, unit = "hour")
+
+  mm$dt <- openair::timeAverage(
+    mm$dt,
+    avg.time = avg.time,
+    start.date = start.date
+  )
+  data.table::setDT(mm$dt) # openair returns a data frame - upgrade to dt
+  # ICOS convention is to use end of interval, and first row pre-dates the start
+  mm$dt <- mm$dt[date >= first_date]
+
+  # restore original time name
+  setnames(mm$dt, "date", eval(time_name))
+  # and delete extra names
+  if (length(wd_name) > 0) {
+    mm$dt[, wd := NULL]
+  }
+  if (length(ws_name) > 0) {
+    mm$dt[, ws := NULL]
+  }
+  return(mm)
 }
--- run_interactive.R
+++ run_interactive.R
@@ -1,11 +1,28 @@
 library(sloop)
 library(devtools)
+library(usethis)
 # usethis::use_package_doc()
+
+# add to .RBuildIgnore
+usethis::use_build_ignore(c("data-raw"))
+
+# # add the packages requiring importing to package.r
+use_tidy_eval()
+use_data_table()
+use_package("data.table", "Depends")
+use_package("fs", "Imports", min_version = "1.5.0")
+use_package("here", "Imports")
+use_package("openair", "Imports")
+use_package("lubridate", "Imports")
+
 
 document()
 build(vignettes = FALSE)
 check(vignettes = FALSE)
 pkgload::load_all()
+
+s3_methods_generic("metamet")
+s3_methods_class("metamet")
 
 # test the constructor
 mm1 <- new_metamet(dt = data.table(x = 1:1000, y = 1:1000))
@@ -13,19 +30,23 @@ mm1
 str(mm1)
 class(mm1)
 
-
 # test the helper
 # test with file paths as argument
-fname_dt <- "data-raw/UK-AMO/UK-AMo_BM_20260126_L03_F02.dat"
+fname_dt <- "data-raw/UK-AMO/UK-AMo_BM_20250822_L03_F02.dat"
 fname_meta <- "data-raw/UK-AMO/dt_meta.csv"
 fname_site <- "data-raw/UK-AMO/dt_site.csv"
-mm <- metamet(fname_dt, dt_meta = fname_meta, dt_site = fname_site)
+mm_from_files <- metamet(
+  dt = fname_dt,
+  dt_meta = fname_meta,
+  dt_site = fname_site
+)
+s3_dispatch(metamet(fname_dt))
 
 # test with data frames as argument
 df <- read.csv(fname_dt)
 df_meta <- read.csv(fname_meta)
 df_site <- read.csv(fname_site)
-mm <- metamet(df, dt_meta = df_meta, dt_site = df_site)
+mm_from_df <- metamet(df, dt_meta = df_meta, dt_site = df_site)
 class(mm)
 str(mm)
 
@@ -33,22 +54,49 @@ str(mm)
 dt <- fread(fname_dt)
 dt_meta <- fread(fname_meta)
 dt_site <- fread(fname_site)
-mm <- metamet(dt, dt_meta = dt_meta, dt_site = dt_site)
-class(mm)
-str(mm)
+mm_from_dt <- metamet(dt, dt_meta = dt_meta, dt_site = dt_site)
+class(mm_from_dt)
+str(mm_from_dt)
+# check all identical - they are not: df differ in class of TIMESTAMP
+identical(mm_from_files$dt, mm_from_df$dt)
+identical(mm_from_files$dt, mm_from_dt$dt)
+identical(mm_from_df$dt, mm_from_dt$dt)
+identical(
+  summary(mm_from_df$dt$D_SNOW_4_1_1),
+  summary(mm_from_dt$dt$D_SNOW_4_1_1)
+)
+
+class(mm_from_df$dt$TIMESTAMP)
+class(mm_from_dt$dt$TIMESTAMP)
+
+# check all identical - they are
+identical(mm_from_files$dt_meta, mm_from_df$dt_meta)
+identical(mm_from_files$dt_meta, mm_from_dt$dt_meta)
+identical(mm_from_df$dt_meta, mm_from_dt$dt_meta)
 
 # test with a mix of arguments
-mm <- metamet(fname_dt, dt_meta = df_meta, dt_site = dt_site)
+mm <- metamet(dt = fname_dt, dt_meta = fname_meta, dt_site = dt_site)
 class(mm)
 str(mm)
+summary(mm$dt)
+dim(mm$dt)
 
+# recreate from files only
+fname_dt <- "data-raw/UK-AMO/UK-AMo_BM_20250822_L03_F02.dat"
+fname_meta <- "data-raw/UK-AMO/dt_meta.csv"
+fname_site <- "data-raw/UK-AMO/dt_site.csv"
+mm <- metamet(
+  dt = fname_dt,
+  dt_meta = fname_meta,
+  dt_site = fname_site
+)
+mm <- time_average(mm, avg.time = "2 hour")
+dim(mm$dt)
+print(mm$dt)
+class(mm)
+str(mm)
+summary(mm$dt)
 
-s3_dispatch(metamet(x))
-s3_methods_generic("metamet")
-s3_methods_class("metamet")
-
-# Example Usage:
-# df <- data.table(a = 1:5)
-# metamet(df)       # Calls metamet.data.frame
-# fwrite(df, "dummy_data.csv")
-# metamet("dummy_data.csv") # Calls metamet.character
+# tested
+mm <- restrict(mm)
+mm <- convert_time_char_to_posix(mm)

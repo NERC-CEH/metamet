06969d506809961a6e70ecaf5597acbabc6a7d10
ADCEH\plevy <plevy@ceh.ac.uk>
2026-04-24 13:05:38 +0100

    moved depends to imports and added importFrom

--- R/metamet-package.r
+++ R/metamet-package.r
@@ -11,5 +11,17 @@
 #' @importFrom data.table .SD
 #' @importFrom data.table :=
 #' @importFrom data.table data.table
+#' @importFrom data.table fread
+#' @importFrom data.table copy
+#' @importFrom data.table setnames
+#' @importFrom data.table setDT
+#' @importFrom data.table setcolorder
+#' @importFrom data.table melt
+#' @importFrom data.table setkeyv
+#' @importFrom data.table shift
+#' @importFrom data.table rbindlist
+#'
+#' @importFrom stats lm na.exclude predict setNames
+#' @importFrom utils head
 ## usethis namespace: end
 NULL

d9b4600b74939a7cf5326ac3d58cfce64ce56c47
ADCEH\plevy <plevy@ceh.ac.uk>
2026-04-30 17:13:18 +0100

    Fixed all errors in R CMD check

--- R/run_shiny.R
+++ R/run_shiny.R
@@ -3,13 +3,20 @@
 #' This function provides a web browser interface.
 #' @keywords shiny app
 #' @return shiny application object
-#' @import shiny
 #' @export
 run_shiny <- function() {
+  if (!requireNamespace("shiny", quietly = TRUE)) {
+    stop(
+      "The 'shiny' package is required to run the metamet app(). ",
+      "Please install it with install.packages('shiny').",
+      call. = FALSE
+    )
+  }
+
   appDir <- system.file("shinyApp", "metqc_app", package = "metamet")
   if (appDir == "") {
     stop(
-      "Could not find example directory. Try re-installing `mypackage`.",
+      "Could not find example directory. Try re-installing `metamet`.",
       call. = FALSE
     )
   }

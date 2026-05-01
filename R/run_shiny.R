9bebc4048a652bf1de9843dcd3113ed5f4b93868
ADCEH\plevy <plevy@ceh.ac.uk>
2026-05-01 12:47:37 +0100

    limit dependency on shiny pkgs and openairmaps to relevant functions

--- R/run_shiny.R
+++ R/run_shiny.R
@@ -5,13 +5,19 @@
 #' @return shiny application object
 #' @export
 run_shiny <- function() {
-  if (!requireNamespace("shiny", quietly = TRUE)) {
-    stop(
-      "The 'shiny' package is required to run the metamet app(). ",
-      "Please install it with install.packages('shiny').",
-      call. = FALSE
-    )
-  }
+  .check_suggests(
+    c(
+      "shiny",
+      "shinydashboard",
+      "shinyjs",
+      "shinyFiles",
+      "shinyvalidate",
+      "shinycssloaders",
+      "ggiraph",
+      "glue"
+    ),
+    context = "the metamet Shiny app"
+  )
 
   appDir <- system.file("shinyApp", "metqc_app", package = "metamet")
   if (appDir == "") {
